9ce58ca1f235783c993a5889079ee24eed38fa00
chynybek2005 <mister.4ika@gmail.com>
2022-12-21 20:49:08 +0900

    exam

---
+++ assets/script/script.js
@@ -0,0 +1,14 @@
+
+const btn = document.getElementById('calc')
+
+const resultParagraph = document.getElementById('result')
+
+btn.addEventListener('click',() => {
+  const soms = document.getElementsByTagName('input')[0].value
+  const dollars = document.getElementsByTagName('input')[1].value
+
+const result = Number(soms) / Number(dollars)
+resultParagraph.innerText = result.toFixed(2)
+
+})
+
